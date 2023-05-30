cc1c90ad4a4ca6a0436b81e8ffc04f0c66450f02
Thailantis <128191487+Thailantis@users.noreply.github.com>
2023-05-30 10:39:13 -0400

    Create item.ts

---
+++ src/item.ts
@@ -0,0 +1,35 @@
+class Item {
+    constructor(name, description, price, uuid) {
+      this.name = name;
+      this.description = description;
+      this.price = price;
+      this.uuid = uuid;
+}
+
+itemElement() {
+    const itemDiv = document.createElement('div');
+    itemDiv.className = 'item';
+
+    const itemName = document.createElement('h3');
+    itemName.textContent = this.name;
+    itemDiv.appendChild(itemName);
+
+    const itemDescription = document.createElement('p');
+    itemDescription.textContent = this.description;
+    itemDiv.appendChild(itemDescription);
+
+    const itemPrice = document.createElement('p');
+    itemPrice.textContent = `Price: $${this.price}`;
+    itemDiv.appendChild(itemPrice);
+
+    const addToCartButton = document.createElement('button');
+    itemPrice.textContent = 'Add to Cart';
+    itemDiv.appendChild(addToCartButton);
+
+    addToCartButton.addEventListener('click', () => {
+        Shop.myUser.addToCart(this);
+        Shop.updateCart();
+    });
+    return itemDiv;
+   }
+}
